f6c335221ef9467b206ed08e8678344cd4f531a4
Michael <contact@michaelknierim.info>
2025-06-30 11:59:16 +0200

    - Changed to 9 players

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ from os import environ
 SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=15)
 
 SESSION_CONFIGS = [
-                   dict(name='Jitsi_Math_HP', num_demo_participants=3, app_sequence=['Intro','mathJitsi', 'HiddenProfile_Jitsi', 'Outro']),
-                   dict(name='Jitsi_HP_Math', num_demo_participants=3, app_sequence=['Intro', 'HiddenProfile_Jitsi', 'mathJitsi', 'Outro']),
-                   dict(name='Chat_Math_HP', num_demo_participants=3, app_sequence=['Intro','mathChat', 'HiddenProfile_Chat', 'Outro']),
-                   dict(name='Chat_HP_Math', num_demo_participants=3, app_sequence=['Intro', 'HiddenProfile_Chat', 'mathChat', 'Outro']),
+                   dict(name='Jitsi_Math_HP', num_demo_participants=9, app_sequence=['Intro','mathJitsi', 'HiddenProfile_Jitsi', 'Outro']),
+                   dict(name='Jitsi_HP_Math', num_demo_participants=9, app_sequence=['Intro', 'HiddenProfile_Jitsi', 'mathJitsi', 'Outro']),
+                   dict(name='Chat_Math_HP', num_demo_participants=9, app_sequence=['Intro','mathChat', 'HiddenProfile_Chat', 'Outro']),
+                   dict(name='Chat_HP_Math', num_demo_participants=9, app_sequence=['Intro', 'HiddenProfile_Chat', 'mathChat', 'Outro']),
                    dict(name='MathJitsiTest', num_demo_participants=3, app_sequence=['mathJitsi'])
                    ]
 
